837b3124ba57df597d94e3bed6f3f067785a3ba9
Will Fisher <will@gina.alaska.edu>
2016-12-29 23:29:15 -0900

    adding some more inspec tests

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -5,3 +5,30 @@ end
 describe directory('/usr/games/minecraft') do
   it { should exist }
 end
+
+describe file('/etc/ssl/certs/mineos.crt') do
+  it { should exist }
+end
+
+describe file('/etc/ssl/certs/mineos.key') do
+  it { should exist }
+end
+
+describe file('/etc/ssl/certs/mineos.pem') do
+  it { should exist }
+end
+
+describe file('/usr/games/minecraft/generate-sslcert.sh') do
+  it { should exist }
+  its('mode') { should cmp '0754' }
+end
+
+describe file('/etc/pm2/conf.d/mineos.json') do
+  it { should exist }
+  its('content') { should match /webui\.js/ }
+end
+
+describe service('pm2') do
+  it { should be_enabled }
+  it { should be_running }
+end
