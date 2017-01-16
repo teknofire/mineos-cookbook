4b12d422343cda3c103bf276c9c1b39d05b0a0fa
Will Fisher <will@gina.alaska.edu>
2017-01-16 13:49:59 -0900

    update java and pm2 cookbook

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+cookbook 'node_pm2', git: 'https://github.com/teknofire/pm2-cookbook.git'
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,1 @@
-if node['platform_family'] == 'debian'
-  if node['platform_version'].to_i == 16
-    default['nodejs']['packages'] = ['nodejs', 'nodejs-legacy', 'npm']
-  end
-end
+default['java']['jdk_version'] = '8'
