295429a16d4ee256401141b4dc4d2b2c260decbf
Will Fisher <wfisher@chef.io>
2018-11-06 16:18:20 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@tekofire.net'
 license 'mit'
 description 'Installs/Configures mineos'
 long_description 'Installs/Configures mineos'
-version '2.0.1'
+version '2.0.2'
 issues_url 'https://github.com/teknofire/mineos-cookbook/issues'
 source_url 'https://github.com/teknofire/mineos-cookbook'
 
